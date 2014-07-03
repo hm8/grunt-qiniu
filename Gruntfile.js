f06bcdea0ff5e9807f0978ed0a8c93f2013bbc41
leoxie <leozone08@gmail.com>
2014-07-03 18:22:26 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/*
+ * grunt-qiniu
+ * https://github.com/leo/grunt-qiniu
+ *
+ * Copyright (c) 2014 leoxie
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // grunt.initConfig({
+  //   qiniu: {
+  //     sync: {
+  //       options: {
+  //         ACCESS_KEY: '',
+  //         SECRET_KEY: '',
+  //         bucket: 'testleo',
+  //         prefix: 'static/',
+  //         path : __dirname
+  //       },
+  //       files: {
+  //         'tmp/default_options': ['dist/static/']
+  //       }
+  //     }
+  //   }
+  // });
+
+  // grunt.loadTasks('tasks');
+};
